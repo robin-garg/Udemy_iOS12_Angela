f57a7e399e274d29c36ec51190bedae05bbaa78f
robingarg86 <robingarg.mca@gmail.com>
2018-11-25 11:17:24 +0530

    D8: Initial project setup Modal created

---
+++ Quizzler-iOS12-master/Quizzler/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var currentQuestion: Question!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currentQuestion = allQuestions.list[0]
+        questionLabel.text = currentQuestion.questionText
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+     }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
