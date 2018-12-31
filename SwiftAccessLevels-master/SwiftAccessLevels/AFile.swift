89ef7d93f8ac597a11dece1c723a609bd377cbb6
robingarg86 <robingarg.mca@gmail.com>
2018-12-31 11:49:37 +0530

    Access Levels - Advanced Swift Part 2

---
+++ SwiftAccessLevels-master/SwiftAccessLevels/AFile.swift
@@ -0,0 +1,53 @@
+//
+//  AFile.swift
+//  SwiftAccessLevels
+//
+//  Created by Angela Yu on 14/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class AClass {
+    
+    //Global variables, also called class properties.
+    private var aPrivateProperty = "private property"
+    
+    fileprivate var aFilePrivateProperty = "fileprivate property"
+    
+    var anInternalProperty = "internal property"
+    
+    func methodA () {
+        
+        let aLocalVariable = "local variable"
+        
+        //Step 1. Try to print aLocalVariable Here - Possible
+        print("\(aLocalVariable) printed from methodA in AClass")
+        
+        //Step 3. Try to print aPrivateProperty Here - Possible
+        
+        //Step 6. Try to print aFilePrivateProperty Here - Possible
+        
+        //Step 9. Try to print anInternalProperty Here - Possible
+    }
+    
+    func methodB () {
+        
+        //Step 2. Try to print aLocalVariable Here - Not Possible
+        
+        //Step 4. Try to print aPrivateProperty Here - Possible
+    }
+    
+}
+
+class AnotherClassInTheSameFile {
+    
+    init() {
+        
+        //Step 5. Try to print aPrivateProperty Here - Not Possible
+        var vc = AClass()
+        //Step 7. Try to print aFilePrivateProperty Here - Possible
+        
+        
+    }
+}
---
+++ SwiftAccessLevels-master/SwiftAccessLevels/AnotherFile.swift
@@ -0,0 +1,21 @@
+//
+//  AnotherFile.swift
+//  SwiftAccessLevels
+//
+//  Created by Angela Yu on 14/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class AnotherClassInAnotherFile {
+    
+    init() {
+        
+        //Step 8. Try to print aFilePrivateProperty Here - Not Possible
+        
+        //Step 10. Try to print anInternalProperty Here - Possible
+        
+    }
+    
+}
