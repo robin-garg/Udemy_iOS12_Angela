67a45cc223719ebc38f15862f3042bff90651c25
robingarg86 <robingarg.mca@gmail.com>
2018-12-31 11:49:05 +0530

    Calculater App UI Updation - Advanced Swift Part 2

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblValue: UILabel!
+    
+    var isFinishedTypingNumber: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else {
+            return
+        }
+        
+        if isFinishedTypingNumber {
+            isFinishedTypingNumber = false
+            lblValue.text = buttonText
+        } else {
+            lblValue.text = lblValue.text! + buttonText
+        }
+    }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+    }
 }
 
