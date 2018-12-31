c157bec0e50e2761237015bbf28991a204136992
robingarg86 <robingarg.mca@gmail.com>
2018-12-31 11:47:46 +0530

    Auto Layout with Stack Views

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Aaryan Garg on 29/12/18.
+//  Copyright © 2018 Aaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
