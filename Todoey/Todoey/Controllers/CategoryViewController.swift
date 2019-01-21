77cd55b9d391825a774d0934da659f489801ba64
robingarg86 <robingarg.mca@gmail.com>
2019-01-21 07:00:14 +0530

    UI improved

--- Todoey/Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -8,97 +8,106 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeableTableViewController {
-    var realm = try! Realm()
-    
-    var arrCategories: Results<Category>?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        reloadData()
-    }
-    
-    func reloadData() {
-        arrCategories = realm.objects(Category.self)
-        tableView.reloadData()
-    }
-    
-    func saveCategory(_ categoryName: String) -> Void {
-        let category = Category()
-        category.name = categoryName
-        
-        do {
-            try realm.write {
-                realm.add(category)
-            }
-        } catch {
-            print("Error while saving category: \(error)")
-        }
-        
-        tableView.reloadData()
-    }
-    
-    override func delete(at indexPath: IndexPath) {
-        if let item = self.arrCategories?[indexPath.row] {
-            do {
-                try self.realm.write {
-                    self.realm.delete(item)
-                }
-            } catch {
-                print("Error deleting object: \(error)")
-            }
-        }
-    }
-
-    @IBAction func onTapAddCategory(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
-        
-        alert.addTextField { (textField) in
-            textField.placeholder = "Enter category name"
-        }
-        
-        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(actionCancel)
-        
-        let actionCreate = UIAlertAction(title: "Create", style: .default) { (action) in
-            if let categoryStr = alert.textFields?.first?.text, categoryStr.count > 0 {
-                self.saveCategory(categoryStr)
-            } else {
-                print("Please enter category name")
-            }
-        }
-        alert.addAction(actionCreate)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    // MARK: - Table view data source
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrCategories?.count ?? 1
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-        
-        // Configure the cell...
-        cell.textLabel?.text = arrCategories?[indexPath.row].name ?? "No Categories found"
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "goToItems", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let todoListController = segue.destination as! TodoListViewController
-        
-        if let indexPath = tableView.indexPathForSelectedRow {
-            todoListController.selectedCateogry = arrCategories?[indexPath.row]
-            tableView.deselectRow(at: indexPath, animated: true)
-        }
-    }
+	var realm = try! Realm()
+	
+	var arrCategories: Results<Category>?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		tableView.rowHeight = 60
+		tableView.separatorStyle = .none
+		
+		reloadData()
+	}
+	
+	func reloadData() {
+		arrCategories = realm.objects(Category.self)
+		tableView.reloadData()
+	}
+	
+	func saveCategory(_ categoryName: String) -> Void {
+		let category = Category()
+		category.name = categoryName
+		category.bgHexColor = UIColor.randomFlat.hexValue()
+		do {
+			try realm.write {
+				realm.add(category)
+			}
+		} catch {
+			print("Error while saving category: \(error)")
+		}
+		
+		tableView.reloadData()
+	}
+	
+	override func delete(at indexPath: IndexPath) {
+		if let item = self.arrCategories?[indexPath.row] {
+			do {
+				try self.realm.write {
+					self.realm.delete(item)
+				}
+			} catch {
+				print("Error deleting object: \(error)")
+			}
+		}
+	}
+	
+	@IBAction func onTapAddCategory(_ sender: UIBarButtonItem) {
+		let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+		
+		alert.addTextField { (textField) in
+			textField.placeholder = "Enter category name"
+		}
+		
+		let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		alert.addAction(actionCancel)
+		
+		let actionCreate = UIAlertAction(title: "Create", style: .default) { (action) in
+			if let categoryStr = alert.textFields?.first?.text, categoryStr.count > 0 {
+				self.saveCategory(categoryStr)
+			} else {
+				print("Please enter category name")
+			}
+		}
+		alert.addAction(actionCreate)
+		present(alert, animated: true, completion: nil)
+	}
+	
+	// MARK: - Table view data source
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return arrCategories?.count ?? 1
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = super.tableView(tableView, cellForRowAt: indexPath)
+		cell.textLabel?.text = "No Categories found"
+		
+		// Configure the cell...
+		if let category = arrCategories?[indexPath.row] {
+			cell.textLabel?.text = category.name
+			let color = UIColor(hexString: category.bgHexColor)
+			cell.backgroundColor = color
+			cell.textLabel?.textColor = ContrastColorOf(color!, returnFlat: true)
+		}
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+		performSegue(withIdentifier: "goToItems", sender: self)
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		let todoListController = segue.destination as! TodoListViewController
+		
+		if let indexPath = tableView.indexPathForSelectedRow {
+			todoListController.selectedCateogry = arrCategories?[indexPath.row]
+			tableView.deselectRow(at: indexPath, animated: true)
+		}
+	}
 }
--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -8,131 +8,164 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeableTableViewController {
-    let itemsKey = "TodoListItemsKey"
-    
-    let realm = try! Realm()
-    
-    var selectedCateogry: Category? {
-        didSet {
-            reloadData()
-        }
-    }
-    var itemArray: Results<Item>?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-    }
-
-    @IBAction func onTapAddItem(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        alert.addTextField { (textField) in
-            textField.placeholder = "Create new item"
-        }
-        let actionAdd = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            guard let itemText = alert.textFields?[0].text else {
-                return
-            }
-            if let category = self.selectedCateogry {
-                do {
-                    try self.realm.write {
-                        let item = Item()
-                        item.title = itemText
-                        item.createdAt = Date()
-                        category.items.append(item)
-                    }
-                } catch {
-                    print("Error saving item")
-                }
-                self.tableView.reloadData()
-            }
-        }
-        alert.addAction(actionAdd)
-        let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(actionCancel)
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func reloadData() {
-        itemArray = selectedCateogry?.items.sorted(byKeyPath: "createdAt", ascending: true)
-
-        tableView.reloadData()
-    }
-    
-    override func delete(at indexPath: IndexPath) {
-        if let item = itemArray?[indexPath.row] {
-            do {
-                try realm.write {
-                    realm.delete(item)
-                }
-            } catch {
-                print("Error updating item")
-            }
-        }
-    }
+	let itemsKey = "TodoListItemsKey"
+	
+	@IBOutlet weak var searchBar: UISearchBar!
+	
+	let realm = try! Realm()
+	
+	var selectedCateogry: Category? {
+		didSet {
+			reloadData()
+		}
+	}
+	var itemArray: Results<Item>?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		tableView.rowHeight = 60
+		tableView.separatorStyle = .none
+		
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		title = selectedCateogry?.name
+		
+		if let color = selectedCateogry?.bgHexColor {
+			updateNavigation(withHexColor: color)
+		}
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		updateNavigation(withHexColor: "FF9300")
+	}
+	
+	func updateNavigation(withHexColor colorHexCode: String) {
+		guard let navBar = navigationController?.navigationBar else { fatalError() }
+		
+		guard let navBarColor = UIColor(hexString: colorHexCode) else { fatalError() }
+		
+		navBar.barTintColor = navBarColor
+		navBar.tintColor = ContrastColorOf(navBarColor, returnFlat: true)
+		navBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: ContrastColorOf(navBarColor, returnFlat: true)]
+		
+		searchBar.barTintColor = navBarColor
+	}
+	
+	@IBAction func onTapAddItem(_ sender: UIBarButtonItem) {
+		let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+		alert.addTextField { (textField) in
+			textField.placeholder = "Create new item"
+		}
+		let actionAdd = UIAlertAction(title: "Add Item", style: .default) { (action) in
+			guard let itemText = alert.textFields?[0].text else {
+				return
+			}
+			if let category = self.selectedCateogry {
+				do {
+					try self.realm.write {
+						let item = Item()
+						item.title = itemText
+						item.createdAt = Date()
+						category.items.append(item)
+					}
+				} catch {
+					print("Error saving item")
+				}
+				self.tableView.reloadData()
+			}
+		}
+		alert.addAction(actionAdd)
+		let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		alert.addAction(actionCancel)
+		
+		present(alert, animated: true, completion: nil)
+	}
+	
+	func reloadData() {
+		itemArray = selectedCateogry?.items.sorted(byKeyPath: "createdAt", ascending: true)
+		
+		tableView.reloadData()
+	}
+	
+	override func delete(at indexPath: IndexPath) {
+		if let item = itemArray?[indexPath.row] {
+			do {
+				try realm.write {
+					realm.delete(item)
+				}
+			} catch {
+				print("Error updating item")
+			}
+		}
+	}
 }
 
 extension TodoListViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray?.count ?? 1
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-        cell.accessoryType = .none
-        
-        if let item = itemArray?[indexPath.row] {
-            cell.textLabel?.text = item.title
-            if item.completed {
-                cell.accessoryType = .checkmark
-            }
-        } else {
-            cell.textLabel?.text = "No Items added yet"
-        }
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
-    }
-        
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView .deselectRow(at: indexPath, animated: true)
-        
-        if let item = itemArray?[indexPath.row] {
-            do {
-                try realm.write {
-//                    realm.delete(item)
-                    item.completed = !item.completed
-                }
-            } catch {
-                print("Error updating item")
-            }
-        }
-        tableView.reloadData()
-    }
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray?.count ?? 1
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = super.tableView(tableView, cellForRowAt: indexPath)
+		cell.accessoryType = .none
+		
+		if let item = itemArray?[indexPath.row] {
+			cell.textLabel?.text = item.title
+			if let color = UIColor(hexString: selectedCateogry!.bgHexColor)?.darken(byPercentage: (CGFloat(indexPath.row) / CGFloat(itemArray!.count))) {
+				cell.backgroundColor = color
+				cell.textLabel?.textColor = ContrastColorOf(color, returnFlat: true)
+			}
+			if item.completed {
+				cell.accessoryType = .checkmark
+			}
+		} else {
+			cell.textLabel?.text = "No Items added yet"
+		}
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+		return .delete
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView .deselectRow(at: indexPath, animated: true)
+		
+		if let item = itemArray?[indexPath.row] {
+			do {
+				try realm.write {
+					//                    realm.delete(item)
+					item.completed = !item.completed
+				}
+			} catch {
+				print("Error updating item")
+			}
+		}
+		tableView.reloadData()
+	}
 }
 
 extension TodoListViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
-        itemArray = itemArray?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "createdAt", ascending: true)
-
-    }
-
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.count == 0 {
-            reloadData()
-
-            DispatchQueue.main.async {
-                searchBar.resignFirstResponder()
-            }
-        }
-    }
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		
+		itemArray = itemArray?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "createdAt", ascending: true)
+		
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		if searchText.count == 0 {
+			reloadData()
+			
+			DispatchQueue.main.async {
+				searchBar.resignFirstResponder()
+			}
+		}
+	}
 }
